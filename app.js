cb3aa12fbc16a7880f9588dc1d608de29f623e19
TheBest101 <lava.mob1001@gmail.com>
2017-05-20 17:13:19 -0400

    add-start

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const moment = require('moment');
+
+var app = express();
+
+app.get('/:id',function(req, res){
+  var date;
+  var num = req.params.id
+  if(/^\d{8,}$/.test(num)){
+    date = moment(num, "X");
+  }else{
+    date = moment(num, "MMMM D, YYYY");
+  }
+
+  if(date.isValid()) {
+    res.json({
+      unix: date.format("X"),
+      natural: date.format("MMMM D, YYYY")
+    });
+  } else {
+    res.json({
+      unix: null,
+      natural: null
+    });
+}
+})
+app.listen(3000, function(){
+  console.log("Working");
+})
