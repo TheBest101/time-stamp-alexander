3be1fa6265c2f504d24833fc4aec4b9b56fee9e3
TheBest101 <lava.mob1001@gmail.com>
2017-05-20 18:09:17 -0400

    make it better

--- app.js
+++ app.js
@@ -25,6 +25,9 @@ app.get('/:id',function(req, res){
     });
   }
 })
+app.get("/", function(req,res){
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
 app.listen(app.get('port'), function(){
   console.log("Working");
 })
